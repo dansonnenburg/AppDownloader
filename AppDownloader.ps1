96fd2b7f8e7bc87197d79046a5e0405b17dd5fee
Dan Sonnenburg <dsonnenburg@organicvalley.com>
2016-08-30 16:10:57 -0500

    Download of files working

--- AppDownloader.ps1
+++ AppDownloader.ps1
@@ -1,10 +1,14 @@
-﻿$URL = "https://notepad-plus-plus.org/repository/6.x/6.9.1/npp.6.9.1.Installer.exe"
-$fileName = ($URL).split('/')[-1]
-$DownloadPath = "C:\Users\dan.sonnenburg\Downloads\PSDownloads\$fileName"
+﻿#$here = (Split-Path -Parent $MyInvocation.MyCommand.Path)
+$here = (Get-Item -Path ".\" -Verbose).FullName
+$json = gc "$here\Download.json" -Raw | ConvertFrom-Json
 
-$Parameters = gc 'MDTDev.json' -Raw | ConvertFrom-Json
-
-Invoke-WebRequest $URL -OutFile $DownloadPath
+# Download all applications in the array
+foreach ( $app in $json.Applications ) {
+    echo $app.URL
+    $fileName = ($app.URL).split('/')[-1]
+    $DownloadPath = "C:\Users\dan.sonnenburg\Downloads\PSDownloads\$fileName"
+    Invoke-WebRequest $app.URL -OutFile $DownloadPath
+}
 
 
 #(Invoke-WebRequest -Uri "https://notepad-plus-plus.org/repository/6.x/").Links.Href | measure -Maximum
