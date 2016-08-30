0fd8d8ad609c2314fb1004190dc68a34881bddf9
Dan Sonnenburg <dsonnenburg@organicvalley.com>
2016-08-30 09:40:40 -0500

    Initial commit

---
+++ AppDownloader.ps1
@@ -0,0 +1,10 @@
+﻿$URL = "https://notepad-plus-plus.org/repository/6.x/6.9.1/npp.6.9.1.Installer.exe"
+$fileName = ($URL).split('/')[-1]
+$DownloadPath = "C:\Users\dan.sonnenburg\Downloads\PSDownloads\$fileName"
+
+$Parameters = gc 'MDTDev.json' -Raw | ConvertFrom-Json
+
+Invoke-WebRequest $URL -OutFile $DownloadPath
+
+
+#(Invoke-WebRequest -Uri "https://notepad-plus-plus.org/repository/6.x/").Links.Href | measure -Maximum
